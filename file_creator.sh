205af371881f6f7dabdd4a06dd051033b4068f2a
lugonzalg <glukas94@gmail.com>
2022-04-16 19:30:48 +0200

    little loading script

--- file_creator.sh
+++ file_creator.sh
@@ -4,6 +4,6 @@ i=0
 j=$(( $2 - 1))
 while [ $i -le $j ]
 do
-	touch $1_0$i.py
+	touch $1_0$i.$3
 	i=$(( $i + 1 ))
 done
---
+++ loading.py
@@ -0,0 +1,21 @@
+from time import sleep, time
+
+def	ft_progress(listy):
+	preffix = "ETA :"
+	ref = time()
+	for i in listy:
+		per = i / listy[-1] * 100
+		bar = "=" * int(per / 5)
+		empty = " " * (20 - len(bar))
+		new_ref = format(time() - ref, ".2f")
+		print(f'\r{preffix} [ {int(per)}%] [{bar}>{empty}] | elapsed time {new_ref}s', end='')
+		yield  i
+
+if __name__ == "__main__":
+	listy = range(1000)
+	ret = 0
+	for elem in ft_progress(listy):
+		ret += (elem + 3) % 5
+		sleep(0.01)
+	print()
+	print(ret)
