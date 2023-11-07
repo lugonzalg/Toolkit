4a339045067ff70b934a68cfd84f7cfc9e36a315
lugonzalg <glukas94@gmail.com>
2023-11-07 19:35:39 +0100

    CHORE: leetcode

---
+++ LeetCode_template.sh
@@ -0,0 +1,20 @@
+cat << EOF > Solution.cpp
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+};
+
+int main() {
+	Solution sl;
+
+	sl.
+
+	return 0;
+}
+
+EOF
